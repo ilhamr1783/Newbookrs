1f430ae07c8c8081d1efbbab1d47f134a6bf7572
ilhamr1783 <ilhamr170803@gmail.com>
2024-06-08 15:57:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,17 @@ if authentication_status == None:
 
 if authentication_status:
 
-    def main():
-    # Indentasi untuk blok kode berikutnya
-    selected = st.sidebar.radio("Navigation", ["Home", "Latar Belakang", "Unique Value", "Buku Untuk Kamu", "Profil Pengembang" ])
+    def streamlit_menu():
+    # horizontal menu w/o custom style
+    selected = option_menu(
+        menu_title=None,  # required
+        options=["Home", "Projects", "Contact"],  # required
+        icons=["house", "book", "envelope"],  # optional
+        menu_icon="cast",  # optional
+        default_index=0,  # optional
+        orientation="horizontal",
+    )
+    return selected
     
     if selected == "Home":
         st.title(f"Selamat Datang Di STARBOOK, A Book Recommendation Website {selected}")
